f238afe71e2c31c0dcc8213043dccd163ec4915f
Luca Stefani <luca.stefani.ge1@gmail.com>
2018-07-15 20:16:49 +0200

    sweet: overlay-lineage: Specify device has a notch
    Change-Id: Ib912728020f85c820b10d5102365dc37f9a54342

--- device.mk
+++ device.mk
@@ -42,7 +42,8 @@ PRODUCT_COPY_FILES += \
 
 # Overlay
 DEVICE_PACKAGE_OVERLAYS += \
-    $(LOCAL_PATH)/overlay
+    $(LOCAL_PATH)/overlay \
+    $(LOCAL_PATH)/overlay-lineage
 
 # Overlay-RRO
 PRODUCT_PACKAGES += \
