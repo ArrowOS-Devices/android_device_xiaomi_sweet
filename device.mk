7b570dca07db524adca9777173dce0dad7f10195
Jiyong Park <jiyong@google.com>
2019-05-18 21:37:03 +0900

    sweet: support updatable APEX
    Before, updatable_apex.mk was inherited indirectly from
    mainline_system.mk. Since mainline_system.mk no longer inherits
    updatable_apex.mk, updatable_apex.mk is inherited directly from
    device-specific mk file.
    
    Bug: 132664085
    Test: choosecombo to phoenix; get_build_var TARGET_FLATTEN_APEX
    shows false
    
    Change-Id: I7162567b732f284f618a77cf09f5b40aadd913d5
    Merged-In: I7162567b732f284f618a77cf09f5b40aadd913d5

--- device.mk
+++ device.mk
@@ -7,6 +7,9 @@
 # Installs gsi keys into ramdisk, to boot a GSI with verified boot.
 $(call inherit-product, $(SRC_TARGET_DIR)/product/gsi_keys.mk)
 
+# Enable updating of APEXes
+$(call inherit-product, $(SRC_TARGET_DIR)/product/updatable_apex.mk)
+
 # API level, the device has been commercially launched on
 PRODUCT_SHIPPING_API_LEVEL := 30
 
