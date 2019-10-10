ea8fecdd6f64eaefe77951c0b0b9ca2efcf4d695
LuK1337 <priv.luk@gmail.com>
2019-10-10 23:04:22 +0200

    sweet: init: Override everything ^.^
    Co-authored-by: Arian <arian.kulmer@web.de>
    Change-Id: I8b617e57ef5345cb55e697c29a63c036512ed0af

--- init/init_sweet.cpp
+++ init/init_sweet.cpp
@@ -29,6 +29,7 @@
 
 #include <fstream>
 #include <unistd.h>
+#include <vector>
 
 #include <android-base/properties.h>
 #define _REALLY_INCLUDE_SYS__SYSTEM_PROPERTIES_H_
@@ -40,22 +41,40 @@
 using android::base::GetProperty;
 using android::init::property_set;
 
+std::vector<std::string> ro_props_default_source_order = {
+    "",
+    "bootimage.",
+    "odm.",
+    "product.",
+    "system.",
+    "system_ext.",
+    "vendor.",
+};
 
-void property_override(char const prop[], char const value[])
+void property_override(char const prop[], char const value[], bool add = true)
 {
     prop_info *pi;
-    pi = (prop_info*) __system_property_find(prop);
+
+    pi = (prop_info *) __system_property_find(prop);
     if (pi)
         __system_property_update(pi, value, strlen(value));
-    else
+    else if (add)
         __system_property_add(prop, strlen(prop), value, strlen(value));
 }
-void property_override_dual(char const system_prop[],
-    char const vendor_prop[], char const value[])
-{
-    property_override(system_prop, value);
-    property_override(vendor_prop, value);
-}
+
+void set_ro_build_prop(const std::string &prop, const std::string &value) {
+    for (const auto &source : ro_props_default_source_order) {
+        auto prop_name = "ro." + source + "build." + prop;
+        property_override(prop_name.c_str(), value.c_str(), false);
+    }
+};
+
+void set_ro_product_prop(const std::string &prop, const std::string &value) {
+    for (const auto &source : ro_props_default_source_order) {
+        auto prop_name = "ro.product." + source + prop;
+        property_override(prop_name.c_str(), value.c_str(), false);
+    }
+};
 
 void vendor_load_properties() {
     std::string region;
@@ -64,22 +83,22 @@ void vendor_load_properties() {
     sku = GetProperty("ro.boot.product.hardware.sku","pro");
 
     if (region == "GLOBAL") {
-        property_override_dual("ro.product.model", "ro.vendor.product.model", "M2101K6G");
-        property_override_dual("ro.product.device", "ro.product.vendor.device", "sweet");
-        property_override_dual("ro.build.fingerprint", "ro.vendor.build.fingerprint", "Redmi/sweet_eea/sweet:11/RKQ1.200826.002/V12.0.10.0.RKFEUXM:user/release-keys");
+        set_ro_product_prop("model", "M2101K6G");
+        set_ro_product_prop("device", "sweet");
+        set_ro_build_prop("fingerprint", "Redmi/sweet_eea/sweet:11/RKQ1.200826.002/V12.0.10.0.RKFEUXM:user/release-keys");
         property_override("ro.build.description", "sweet_eea-user 11 RKQ1.200826.002 V12.0.10.0.RKFEUXM release-keys");
         property_override("ro.product.mod_device", "sweet_eea_global");
     } else if (region == "INDIA") {
         if (sku == "std") {
-            property_override_dual("ro.product.model", "ro.vendor.product.model", "M2101K6P");
-            property_override_dual("ro.product.device", "ro.product.vendor.device",  "sweetin");
-            property_override_dual("ro.build.fingerprint", "ro.vendor.build.fingerprint", "Redmi/sweetin/sweetin:11/RKQ1.200826.002/V12.0.6.0.RKFINXM:user/release-keys");
+            set_ro_product_prop("model", "M2101K6P");
+            set_ro_product_prop("device",  "sweetin");
+            set_ro_build_prop("fingerprint", "Redmi/sweetin/sweetin:11/RKQ1.200826.002/V12.0.6.0.RKFINXM:user/release-keys");
             property_override("ro.build.description", "sweetin-user 11 RKQ1.200826.002 V12.0.6.0.RKFINXM release-keys");
             property_override("ro.product.mod_device", "sweetin_in_global");
             } else {
-                property_override_dual("ro.product.model", "ro.vendor.product.model", "M2101K6I");
-                property_override_dual("ro.product.device", "ro.product.vendor.device",  "sweetin");
-                property_override_dual("ro.build.fingerprint", "ro.vendor.build.fingerprint", "Redmi/sweetin/sweetin:11/RKQ1.200826.002/V12.0.6.0.RKFINXM:user/release-keys");
+                set_ro_product_prop("model", "M2101K6I");
+                set_ro_product_prop("device",  "sweetin");
+                set_ro_build_prop("fingerprint", "Redmi/sweetin/sweetin:11/RKQ1.200826.002/V12.0.6.0.RKFINXM:user/release-keys");
                 property_override("ro.build.description", "sweetin-user 11 RKQ1.200826.002 V12.0.6.0.RKFINXM release-keys");
                 property_override("ro.product.mod_device", "sweetin_in_global");
             }
