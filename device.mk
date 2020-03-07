9fea1b3b93fbc02bb0dbba7977adcf163b8badb2
Wei Wang <wvw@google.com>
2020-03-06 16:49:16 -0800

    sweet: import power-libperfmgr and libperfmgr
    * From hardware/google/pixel (android-11.0.0_r35)
    * Powerhint from sunfish (android-11.0.0_r35)
    
    Change-Id: Ie037f55bfbef59451ea1634d5cdf36a25c091736
    Signed-off-by: daniml3 <danimoral1001@gmail.com>

--- device.mk
+++ device.mk
@@ -90,5 +90,12 @@ PRODUCT_PACKAGES += \
 PRODUCT_SOONG_NAMESPACES += \
     $(LOCAL_PATH)
 
+# Power
+PRODUCT_PACKAGES += \
+    android.hardware.power-service.sweet-libperfmgr
+
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/power-libperfmgr/powerhint.json:$(TARGET_COPY_OUT_PRODUCT)/vendor_overlay/$(PRODUCT_TARGET_VNDK_VERSION)/etc/powerhint.json
+
 include vendor/xiaomi/sweet/sweet-vendor.mk
 
