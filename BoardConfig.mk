266f00f87660ec0b4d04b351c156e37d697ab06a
TheScarastic <warabhishek@gmail.com>
2020-04-22 10:50:01 +0000

    sweet: Initial sepolicy
    Change-Id: I53e24bea6929e992de9ba255b57a7c48476dac46

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -29,8 +29,7 @@ BOARD_KERNEL_CMDLINE := androidboot.hardware=qcom \
                         video=vfb:640x400,bpp=32,memsize=3072000 msm_rtb.filter=0x237 \
                         service_locator.enable=1 \
                         swiotlb=1 \
-                        androidboot.usbcontroller=a600000.dwc3 \
-                        loop.max_part=7 androidboot.selinux=permissive
+                        androidboot.usbcontroller=a600000.dwc3
 
 BOARD_KERNEL_BASE        := 0x00000000
 BOARD_KERNEL_PAGESIZE    := 4096
