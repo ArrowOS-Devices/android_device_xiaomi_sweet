b0dadb8019848e5331c02f4d8512fbd3e4fc79c2
Arian <arian.kulmer@web.de>
2021-03-14 18:21:54 +0100

    sweet: Configure partitions
    Change-Id: I802acf41be02788cb14646fbcfc2e9bbdb1f99f9

--- device.mk
+++ device.mk
@@ -21,6 +21,10 @@ PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/rootdir/etc/fstab.qcom:$(TARGET_COPY_OUT_RAMDISK)/fstab.qcom \
     $(LOCAL_PATH)/rootdir/etc/fstab.qcom:$(TARGET_COPY_OUT_VENDOR)/etc/fstab.qcom
 
+# Partitions
+PRODUCT_BUILD_SUPER_PARTITION := false
+PRODUCT_USE_DYNAMIC_PARTITIONS := true
+
 # Soong namespaces
 PRODUCT_SOONG_NAMESPACES += \
     $(LOCAL_PATH)
