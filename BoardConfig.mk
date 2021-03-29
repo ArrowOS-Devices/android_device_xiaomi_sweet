d0acdf545c6a6b8a33ae61bf9104519f892a135a
Ethan Chen <intervigil@gmail.com>
2021-03-29 14:39:37 +0200

    sweet: Disable verity and descriptor checking
    * avbtool provides an option to disable hashtree checking
    
    Change-Id: Ib57d8ebed69a0998005563a9268f9fc736cbeaf7

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -79,6 +79,7 @@ BOARD_VNDK_VERSION := current
 
 # Verified Boot
 BOARD_AVB_ENABLE := true
+BOARD_AVB_MAKE_VBMETA_IMAGE_ARGS += --set_hashtree_disabled_flag
 BOARD_AVB_MAKE_VBMETA_IMAGE_ARGS += --flags 2
 
 BOARD_AVB_RECOVERY_KEY_PATH := external/avb/test/data/testkey_rsa2048.pem
