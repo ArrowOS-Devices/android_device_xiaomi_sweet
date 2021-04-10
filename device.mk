0600b9eee69254bcaf124d7ce6f2bfd9cc0f6c75
Luca Stefani <luca.stefani.ge1@gmail.com>
2021-04-10 15:23:14 +0200

    sweet: build the AOSP NFC stack
    Change-Id: I049c285fca52b89c5352985e03af3ecd4bb83291

--- device.mk
+++ device.mk
@@ -39,4 +39,11 @@ PRODUCT_PACKAGES += \
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/vintf/android.hardware.lights-qti.xml:$(TARGET_COPY_OUT_SYSTEM)/etc/android.hardware.lights-qti.xml
 
+# NFC
+PRODUCT_PACKAGES += \
+    NfcNci \
+    Tag \
+    SecureElement \
+    com.android.nfc_extras
+
 include vendor/xiaomi/sweet/sweet-vendor.mk
