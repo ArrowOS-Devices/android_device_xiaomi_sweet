594d9d416961f3eb0e5b88a83879e323e3039167
AdarshGrewal <adarshgrewal@gmail.com>
2021-04-13 13:50:17 +0200

    sweet: build telephony packages
    Change-Id: I2fa801e3e9985bad1fa2f189982ca39d46715eee

--- device.mk
+++ device.mk
@@ -58,4 +58,21 @@ PRODUCT_PACKAGES += \
 DEVICE_PACKAGE_OVERLAYS += \
     $(LOCAL_PATH)/overlay
 
+# Permissions
+PRODUCT_COPY_FILES += \
+    frameworks/native/data/etc/android.hardware.telephony.ims.xml:$(TARGET_COPY_OUT_PRODUCT)/etc/permissions/android.hardware.telephony.ims.xml
+
+# Telephony
+PRODUCT_PACKAGES += \
+    ims-ext-common \
+    ims_ext_common.xml \
+    qti-telephony-hidl-wrapper \
+    qti_telephony_hidl_wrapper.xml \
+    qti-telephony-utils \
+    qti_telephony_utils.xml \
+    telephony-ext \
+
+PRODUCT_BOOT_JARS += \
+    telephony-ext
+
 include vendor/xiaomi/sweet/sweet-vendor.mk
