327fa5e3390163e76cee38944a04588b799814da
AdarshGrewal <adarsh@aosip.dev>
2021-04-14 19:14:57 +0530

    sweet: Add headphone button jack keylayout
    Change-Id: I7b645304615d9066dbf36015020d275159408bcd

--- device.mk
+++ device.mk
@@ -38,6 +38,11 @@ PRODUCT_PACKAGES += \
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/rootdir/etc/init.sweet.rc:$(TARGET_COPY_OUT_SYSTEM)/etc/init/init.sweet.rc
 
+# Keylayout
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/configs/keylayout/gpio-keys.kl:$(TARGET_COPY_OUT_SYSTEM)/usr/keylayout/gpio-keys.kl \
+    $(LOCAL_PATH)/configs/keylayout/sm6150-idp-snd-card_Button_Jack.kl:$(TARGET_COPY_OUT_SYSTEM)/usr/keylayout/sm6150-idp-snd-card_Button_Jack.kl
+
 # Lights
 PRODUCT_PACKAGES += \
     android.hardware.lights-service.sweet
