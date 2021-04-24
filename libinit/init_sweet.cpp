206493125dae734d993fdd2f1fd59375d82c9c07
daniml3 <danimoral1001@gmail.com>
2021-04-25 00:19:31 +0200

    sweet: improve recovery behavior on non-user builds
    * Always enable ro.debuggable
    * Enable adb by default
    
    Signed-off-by: daniml3 <danimoral1001@gmail.com>
    Change-Id: I5d3bf5d8c1bbd3dda29d27a2f1e9fbd3d1d5383a

--- libinit/init_sweet.cpp
+++ libinit/init_sweet.cpp
@@ -69,4 +69,12 @@ void vendor_load_properties() {
     full_property_override("build.fingerprint", fingerprint);
     full_property_override("build.description", description);
     property_override("ro.boot.verifiedbootstate", "green");
+
+#ifdef __ANDROID_RECOVERY__
+    std::string buildtype = GetProperty("ro.build.type", "userdebug");
+    if (buildtype != "user") {
+        property_override("ro.debuggable", "1");
+        property_override("ro.adb.secure.recovery", "0");
+    }
+#endif
 }
