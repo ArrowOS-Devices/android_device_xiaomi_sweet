ca7eeafeb3c2b71bb97127a1969d8f8f5787b7dd
daniml3 <danimoral1001@gmail.com>
2021-04-27 10:20:20 +0200

    sweet: lights: turn into an AIDL service
    * Based on AOSP's default lights AIDL implementation
    * Remove unused functions
    * Remove unused lights
    * Refactor specific lights handling
    * Stop overriding the stock lights HAL vintf entry
    
    Signed-off-by: daniml3 <danimoral1001@gmail.com>
    Change-Id: I92a55e9a0dd9a5dcebc7ec1653dc7aca6126b34d

--- device.mk
+++ device.mk
@@ -40,11 +40,7 @@ PRODUCT_COPY_FILES += \
 
 # Lights
 PRODUCT_PACKAGES += \
-    android.hardware.light@2.0-service.sweet
-
-# VINTF
-PRODUCT_COPY_FILES += \
-    $(LOCAL_PATH)/configs/vintf/android.hardware.lights-qti.xml:$(TARGET_COPY_OUT_PRODUCT)/vendor_overlay/$(PRODUCT_TARGET_VNDK_VERSION)/etc/vintf/manifest/android.hardware.lights-qti.xml
+    android.hardware.lights-service.sweet
 
 # NFC
 PRODUCT_PACKAGES += \
