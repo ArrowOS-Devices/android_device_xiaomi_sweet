143893cc8492a65f030db9f31bdc98b8cac09e0b
daniml3 <daniel@danielml.dev>
2021-04-29 19:19:36 +0200

    sweet: initial support for included vendor and odm
    * VINTF manifest, compatibility matrix and vendor properties from MIUI
    * Make QCOM_SOONG_NAMESPACE empty to opt-out from OSS QCOM HALs
    
    Change-Id: I72cd9d4efa8aa75851d9dc69bd156742545a5f27

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -1,6 +1,9 @@
 DEVICE_PATH := device/xiaomi/sweet
 BOARD_VENDOR := xiaomi
 
+BUILD_BROKEN_DUP_RULES := true
+QCOM_SOONG_NAMESPACE := $(empty)
+
 # Architecture
 TARGET_ARCH := arm64
 TARGET_ARCH_VARIANT := armv8-a
@@ -29,7 +32,8 @@ BOARD_KERNEL_CMDLINE := androidboot.hardware=qcom \
                         video=vfb:640x400,bpp=32,memsize=3072000 msm_rtb.filter=0x237 \
                         service_locator.enable=1 \
                         swiotlb=1 \
-                        androidboot.usbcontroller=a600000.dwc3
+                        androidboot.usbcontroller=a600000.dwc3 \
+                        androidboot.selinux=permissive
 
 BOARD_KERNEL_BASE        := 0x00000000
 BOARD_KERNEL_PAGESIZE    := 4096
@@ -41,12 +45,13 @@ BOARD_MKBOOTIMG_ARGS := --header_version $(BOARD_BOOTIMG_HEADER_VERSION)
 TARGET_PREBUILT_KERNEL := $(DEVICE_PATH)/prebuilt/Image
 TARGET_PREBUILT_DTB := $(DEVICE_PATH)/prebuilt/dtb
 TARGET_KERNEL_ARCH := arm64
+TARGET_KERNEL_HEADERS := kernel/xiaomi/sweet
 BOARD_MKBOOTIMG_ARGS += --dtb $(TARGET_PREBUILT_DTB)
 
 # Partitions
 BOARD_SUPER_PARTITION_SIZE := 9126805504
 BOARD_SUPER_PARTITION_GROUPS := qti_dynamic_partitions
-BOARD_QTI_DYNAMIC_PARTITIONS_PARTITION_LIST := product system system_ext
+BOARD_QTI_DYNAMIC_PARTITIONS_PARTITION_LIST := odm product system system_ext vendor
 BOARD_QTI_DYNAMIC_PARTITIONS_SIZE := 9122611200
 
 BOARD_FLASH_BLOCK_SIZE := 131072
@@ -58,11 +63,13 @@ BOARD_RECOVERYIMAGE_PARTITION_SIZE := 134217728
 
 BOARD_PARTITION_LIST := $(call to-upper, $(BOARD_QTI_DYNAMIC_PARTITIONS_PARTITION_LIST))
 $(foreach p, CACHE $(BOARD_PARTITION_LIST), $(eval BOARD_$(p)IMAGE_FILE_SYSTEM_TYPE := ext4))
-$(foreach p, $(BOARD_PARTITION_LIST) VENDOR, $(eval TARGET_COPY_OUT_$(p) := $(call to-lower, $(p))))
+$(foreach p, $(BOARD_PARTITION_LIST), $(eval TARGET_COPY_OUT_$(p) := $(call to-lower, $(p))))
 $(foreach p, $(BOARD_PARTITION_LIST), $(eval BOARD_$(p)IMAGE_PARTITION_RESERVED_SIZE := 524288000))
 
 TARGET_USERIMAGES_USE_F2FS := true
 TARGET_USERIMAGES_USE_EXT4 := true
+
+VENDOR_SECURITY_PATCH := 2021-05-01
 
 # Platform
 TARGET_BOARD_PLATFORM := $(TARGET_BOOTLOADER_BOARD_NAME)
@@ -105,14 +112,18 @@ BOARD_PLAT_PUBLIC_SEPOLICY_DIR += $(DEVICE_PATH)/sepolicy/public
 include device/qcom/sepolicy/SEPolicy.mk
 
 # VINTF
+DEVICE_MANIFEST_FILE := $(DEVICE_PATH)/configs/vintf/manifest.xml
 DEVICE_FRAMEWORK_MANIFEST_FILE := $(DEVICE_PATH)/configs/vintf/framework_manifest.xml
+DEVICE_FRAMEWORK_COMPATIBILITY_MATRIX_FILE := $(DEVICE_PATH)/configs/vintf/compatibility_matrix.xml
 
 # Init
 TARGET_INIT_VENDOR_LIB := //$(DEVICE_PATH):libinit_sweet
 TARGET_RECOVERY_DEVICE_MODULES := libinit_sweet
 
 # Properties
+TARGET_ODM_PROP += $(DEVICE_PATH)/odm.prop
 TARGET_SYSTEM_PROP += $(DEVICE_PATH)/system.prop
+TARGET_VENDOR_PROP += $(DEVICE_PATH)/vendor.prop
 
 # FM
 BOARD_HAVE_QCOM_FM := true
