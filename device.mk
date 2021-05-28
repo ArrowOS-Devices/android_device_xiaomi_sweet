3c1910f0ce5850cbbf754c7d99007d71f3451fe4
daniml3 <danimoral1001@gmail.com>
2021-05-28 15:24:11 +0200

    sweet: build the display stack from source
    * PRODUCT_PACKAGES definitions from CAF's display product makefile
    * Remove existing VINTF entries as those are fragments now
    * Remove Xiaomi's displayfeature as its unneeded
    
    Signed-off-by: daniml3 <danimoral1001@gmail.com>
    Change-Id: I7768e59d4387120eec72092cdf609b3deae75c40

--- device.mk
+++ device.mk
@@ -116,5 +116,32 @@ PRODUCT_PACKAGES += \
     android.hardware.camera.provider@2.4-impl \
     android.hardware.camera.provider@2.4-service_64
 
+# Display
+PRODUCT_PACKAGES += \
+    android.hardware.graphics.composer@2.4-impl \
+    android.hardware.graphics.composer@2.4-service \
+    android.hardware.graphics.mapper@3.0-impl-qti-display \
+    android.hardware.graphics.mapper@4.0-impl-qti-display \
+    vendor.qti.hardware.display.allocator-service \
+    android.hardware.memtrack@1.0-impl \
+    android.hardware.memtrack@1.0-service \
+    gralloc.sm6150 \
+    hwcomposer.sm6150 \
+    memtrack.sm6150 \
+    vendor.display.config@1.0.vendor \
+    vendor.display.config@1.1.vendor \
+    vendor.display.config@1.2.vendor \
+    vendor.display.config@1.3.vendor \
+    vendor.display.config@1.4.vendor \
+    vendor.display.config@1.5.vendor \
+    vendor.display.config@1.6.vendor \
+    vendor.display.config@1.7.vendor \
+    vendor.display.config@1.8.vendor \
+    vendor.display.config@1.9.vendor \
+    vendor.display.config@1.10.vendor \
+    vendor.display.config@2.0.vendor \
+    vendor.qti.hardware.display.mapper@3.0 \
+    vendor.qti.hardware.display.mapper@4.0.vendor
+
 include vendor/xiaomi/sweet/sweet-vendor.mk
 
