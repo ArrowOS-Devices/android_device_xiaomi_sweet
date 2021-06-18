e9d17e7e1d2d86a68d04703562ce645299e69002
daniml3 <danimoral1001@gmail.com>
2021-06-19 00:56:53 +0200

    sweet: switch from build time overlays to RROs
    Signed-off-by: daniml3 <danimoral1001@gmail.com>
    Change-Id: Ibf585f5c870765d824edbf892d118c5110bfb768

--- device.mk
+++ device.mk
@@ -62,8 +62,13 @@ PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/devicesettings/privapp-permissions-devicesettings.xml:$(TARGET_COPY_OUT_SYSTEM)/etc/permissions/privapp-permissions-devicesettings.xml
 
 # Overlays
-DEVICE_PACKAGE_OVERLAYS += \
-    $(LOCAL_PATH)/overlay
+PRODUCT_PACKAGES += \
+    CarrierConfigOverlaySweet \
+    DialerOverlaySweet \
+    FrameworkResOverlaySweet \
+    SettingsProviderOverlaySweet \
+    SystemUIOverlaySweet \
+    TelephonyOverlaySweet
 
 # Permissions
 PRODUCT_COPY_FILES += \
@@ -120,4 +125,3 @@ PRODUCT_PACKAGES += \
     libshim_megvii
 
 include vendor/xiaomi/sweet/sweet-vendor.mk
-
