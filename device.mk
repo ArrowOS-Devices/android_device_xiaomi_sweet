13e13328b882539001631219e0b5ab6aec0cd0d1
daniml3 <danimoral1001@gmail.com>
2021-06-26 13:59:06 +0200

    sweet: build QTI perf interfaces from source
    Signed-off-by: daniml3 <danimoral1001@gmail.com>
    Change-Id: I7205c7c99317477e4c36b54763a4e1c738217de7

--- device.mk
+++ device.mk
@@ -220,4 +220,8 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     vendor.xiaomi.hardware.touchfeature@1.0.vendor
 
+# Perf
+PRODUCT_PACKAGES += \
+    vendor.qti.hardware.perf@2.2.vendor
+
 include vendor/xiaomi/sweet/sweet-vendor.mk
