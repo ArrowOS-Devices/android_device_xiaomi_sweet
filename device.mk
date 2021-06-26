835bb49bdf48ad463c3aae529c0ed689d3e07be4
daniml3 <danimoral1001@gmail.com>
2021-06-26 18:56:42 +0200

    sweet: build AOSP and QTI thermal service from source
    Signed-off-by: daniml3 <danimoral1001@gmail.com>
    Change-Id: I8ced56da492c43a3fbef7d1016c837c34200714c

--- device.mk
+++ device.mk
@@ -237,4 +237,10 @@ PRODUCT_PACKAGES += \
     wpa_supplicant \
     wpa_supplicant.conf
 
+# Thermal
+PRODUCT_PACKAGES += \
+    android.hardware.thermal@1.0-service \
+    android.hardware.thermal@1.0-impl \
+    android.hardware.thermal@2.0-service.qti
+
 include vendor/xiaomi/sweet/sweet-vendor.mk
