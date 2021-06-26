bef3cfcb8ba411638d2099b596faa2c657497f77
daniml3 <danimoral1001@gmail.com>
2021-06-26 18:56:42 +0200

    sweet: build the QTI thermal service from source
    Signed-off-by: daniml3 <danimoral1001@gmail.com>
    Change-Id: I8ced56da492c43a3fbef7d1016c837c34200714c

--- device.mk
+++ device.mk
@@ -236,4 +236,9 @@ PRODUCT_PACKAGES += \
     wpa_supplicant \
     wpa_supplicant.conf
 
+# Thermal
+PRODUCT_PACKAGES += \
+    android.hardware.thermal@1.0-impl \
+    android.hardware.thermal@2.0-service.qti
+
 include vendor/xiaomi/sweet/sweet-vendor.mk
