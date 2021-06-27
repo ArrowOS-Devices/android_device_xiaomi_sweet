880ded0672c788c7f4099d252f05bde420488a41
daniml3 <daniel@danielml.dev>
2021-06-27 22:29:38 +0200

    sweet: build the QTI WiFi interfaces from source
    Signed-off-by: daniml3 <daniel@danielml.dev>
    Change-Id: I962dda49db3ec3f263ed8e2c41414f5668f53397

--- device.mk
+++ device.mk
@@ -260,6 +260,14 @@ PRODUCT_PACKAGES += \
     wpa_supplicant \
     wpa_supplicant.conf
 
+PRODUCT_PACKAGES += \
+    vendor.qti.hardware.wifi.hostapd@1.0.vendor \
+    vendor.qti.hardware.wifi.hostapd@1.1.vendor \
+    vendor.qti.hardware.wifi.hostapd@1.2.vendor \
+    vendor.qti.hardware.wifi.supplicant@2.0.vendor \
+    vendor.qti.hardware.wifi.supplicant@2.1.vendor \
+    vendor.qti.hardware.wifi.supplicant@2.2.vendor
+
 # Thermal
 PRODUCT_PACKAGES += \
     android.hardware.thermal@1.0-impl \
