9828e318ecca2edb49cc52be712990a25f28342e
daniml3 <danimoral1001@gmail.com>
2021-06-27 13:04:26 +0200

    sweet: build librmnetctl from source
    Signed-off-by: daniml3 <danimoral1001@gmail.com>
    Change-Id: I68754d0238dacb5d8ab5f500f8211ea6c8461171

--- device.mk
+++ device.mk
@@ -289,4 +289,8 @@ PRODUCT_PACKAGES += \
     android.hardware.sensors@1.0-service \
     android.hardware.sensors@1.0-impl
 
+# Mobile data
+PRODUCT_PACKAGES += \
+    librmnetctl
+
 include vendor/xiaomi/sweet/sweet-vendor.mk
