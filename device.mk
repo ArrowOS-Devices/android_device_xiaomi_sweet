6078451f63dfba8197150be78bb4df601c42c811
daniml3 <danimoral1001@gmail.com>
2021-06-28 20:38:39 +0200

    sweet: build QTI bluetooth {interfaces, configstore} from source
    Signed-off-by: daniml3 <danimoral1001@gmail.com>
    Change-Id: I840e59954c59967d2980c15a5cbf87568ad5e942

--- device.mk
+++ device.mk
@@ -330,4 +330,12 @@ PRODUCT_PACKAGES += \
     vendor.qti.hardware.vibrator.impl \
     vibrator.default
 
+# Bluetooth
+PRODUCT_PACKAGES += \
+    vendor.qti.hardware.btconfigstore@1.0.vendor \
+    vendor.qti.hardware.btconfigstore@2.0.vendor \
+    vendor.qti.hardware.bluetooth_audio@2.0 \
+    com.qualcomm.qti.bluetooth_audio@1.0 \
+    android.hardware.bluetooth.audio@2.0-impl
+
 include vendor/xiaomi/sweet/sweet-vendor.mk
