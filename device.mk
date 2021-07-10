57864dabce63317cd1c7d91ff93b25e32cd7b828
AdarshGrewal <adarshgrewal@gmail.com>
2021-07-10 16:44:38 +0530

    sweet: Declare shipping API level

--- device.mk
+++ device.mk
@@ -4,6 +4,9 @@
 # SPDX-License-Identifier: Apache-2.0
 #
 
+# API level, the device has been commercially launched on
+PRODUCT_SHIPPING_API_LEVEL := 30
+
 # Soong namespaces
 PRODUCT_SOONG_NAMESPACES += \
     $(LOCAL_PATH)
