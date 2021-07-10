1b074efa781f88e29d35a09ee43e9b2301e5a98d
AdarshGrewal <adarshgrewal@gmail.com>
2021-07-10 18:46:26 +0530

    sweet: define screen density

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -59,6 +59,9 @@ TARGET_VENDOR_PROP += $(DEVICE_PATH)/vendor.prop
 # Recovery
 TARGET_RECOVERY_FSTAB := $(DEVICE_PATH)/rootdir/etc/fstab.qcom
 
+# Screen density
+TARGET_SCREEN_DENSITY := 440
+
 # Sepolicy
 BOARD_VENDOR_SEPOLICY_DIRS += $(DEVICE_PATH)/sepolicy/vendor
 
