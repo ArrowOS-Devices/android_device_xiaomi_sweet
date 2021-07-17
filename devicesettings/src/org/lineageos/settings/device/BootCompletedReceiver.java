d1d06253f39772c017ecc545c2c1fe49fe567a48
daniml3 <danimoral1001@gmail.com>
2021-07-18 00:16:37 +0200

    sweet: devicesettings: remove double tap to wake handling
    * Mark the package as not persistent as its unneeded now
    
    * Also remove Xiaomi's touchfeature service as its unused
    
    Signed-off-by: daniml3 <danimoral1001@gmail.com>
    Change-Id: I56fb6464da21c2185779af534220627b2a786401

--- devicesettings/src/org/lineageos/settings/device/BootCompletedReceiver.java
+++ devicesettings/src/org/lineageos/settings/device/BootCompletedReceiver.java
@@ -21,10 +21,8 @@ import android.content.BroadcastReceiver;
 import android.content.ComponentName;
 import android.content.Context;
 import android.content.Intent;
-import android.content.ContentResolver;
 import android.content.pm.PackageManager;
 import android.content.SharedPreferences;
-import android.database.ContentObserver;
 import android.os.Handler;
 
 import androidx.preference.PreferenceManager;
@@ -34,8 +32,6 @@ import org.lineageos.settings.device.dirac.DiracUtils;
 import org.lineageos.settings.device.utils.DisplayUtils;
 import org.lineageos.settings.device.utils.FileUtils;
 import org.lineageos.settings.device.utils.KcalUtils;
-
-import vendor.xiaomi.hardware.touchfeature.V1_0.ITouchFeature;
 
 public class BootCompletedReceiver extends BroadcastReceiver {
 
@@ -49,32 +45,11 @@ public class BootCompletedReceiver extends BroadcastReceiver {
         Settings.System.putFloat(context.getContentResolver(),
             Settings.System.MIN_REFRESH_RATE, refreshRate);
 
-        ContentObserver observer = new ContentObserver(Handler.getMain()) {
-            @Override
-            public void onChange(boolean selfChange) {
-                updateTapToWakeStatus(context);
-            }
-        };
-
-        context.getContentResolver().registerContentObserver(
-            Settings.Secure.getUriFor(Settings.Secure.DOUBLE_TAP_TO_WAKE), true, observer);
-
-        updateTapToWakeStatus(context);
         DisplayUtils.setDcDimmingStatus(sharedPreferences.getBoolean(Constants.KEY_DC_DIMMING, false));
         DiracUtils.initialize(context);
 
         if (KcalUtils.isKcalSupported()) {
             KcalUtils.writeCurrentSettings(sharedPreferences);
         }
-    }
-
-    private void updateTapToWakeStatus(Context context) {
-       try {
-            ITouchFeature.getService().setTouchMode(Constants.DT2W_TOUCH_FEATURE,
-                    (Settings.Secure.getInt(context.getContentResolver(),
-                            Settings.Secure.DOUBLE_TAP_TO_WAKE, 0) == 1) ? 1 : 0);
-       } catch (Exception e) {
-           e.printStackTrace();
-       }
     }
 }
