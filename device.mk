ae9adc5de5f829626f323d9c2c8c1d3ffc8a286f
daniml3 <danimoral1001@gmail.com>
2021-08-08 13:14:12 +0200

    sweet: import stock audio_platform_info_intcodec
    Signed-off-by: daniml3 <danimoral1001@gmail.com>
    Change-Id: I534bd0dba2c74be106c3f4c68cd7e84ddf98b96c

--- device.mk
+++ device.mk
@@ -12,7 +12,8 @@ PRODUCT_AAPT_PREF_CONFIG := xxhdpi
 
 # Audio
 PRODUCT_COPY_FILES += \
-    $(LOCAL_PATH)/configs/audio/audio_policy_configuration.xml:$(TARGET_COPY_OUT_VENDOR)/etc/audio_policy_configuration.xml
+    $(LOCAL_PATH)/configs/audio/audio_policy_configuration.xml:$(TARGET_COPY_OUT_VENDOR)/etc/audio_policy_configuration.xml \
+    $(LOCAL_PATH)/configs/audio/audio_platform_info_intcodec.xml:$(TARGET_COPY_OUT_VENDOR)/etc/audio_platform_info_intcodec.xml
 
 PRODUCT_PACKAGES += \
     libvolumelistener \
