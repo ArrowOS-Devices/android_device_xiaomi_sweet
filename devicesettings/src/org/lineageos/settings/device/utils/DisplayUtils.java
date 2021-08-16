eb9c73f95a9daae1102a482078fe5092b6bb345d
daniml3 <danimoral1001@gmail.com>
2021-08-17 01:30:53 +0200

    sweet: devicesettings: allow to change the peak refresh rate
    Signed-off-by: daniml3 <danimoral1001@gmail.com>
    Change-Id: I6b1a4749c45e8a13185589ded8a2069980cab506

--- devicesettings/src/org/lineageos/settings/device/utils/DisplayUtils.java
+++ devicesettings/src/org/lineageos/settings/device/utils/DisplayUtils.java
@@ -1,5 +1,13 @@
 package org.lineageos.settings.device.utils;
 
+import android.content.SharedPreferences;
+
+import android.content.Context;
+import android.os.Handler;
+import android.provider.Settings;
+
+import androidx.preference.PreferenceManager;
+
 import org.lineageos.settings.device.Constants;
 import org.lineageos.settings.device.utils.FileUtils;
 
@@ -9,5 +17,18 @@ public class DisplayUtils {
 
         // Update the brightness node so dc dimming updates its state
         FileUtils.writeLine(Constants.BRIGHTNESS_NODE, FileUtils.readOneLine(Constants.BRIGHTNESS_NODE));
+    }
+
+    public static void updateRefreshRateSettings(final Context context) {
+        Handler.getMain().post(() -> {
+        SharedPreferences sharedPreferences = PreferenceManager.getDefaultSharedPreferences(context);
+        String[] refreshRateSettings = sharedPreferences.getString(
+            Constants.KEY_REFRESH_RATE_CONFIG, Constants.DEFAULT_REFRESH_RATE_CONFIG).split("-");
+
+        Settings.System.putFloat(context.getContentResolver(),
+            Settings.System.MIN_REFRESH_RATE, Float.valueOf(refreshRateSettings[0]));
+        Settings.System.putFloat(context.getContentResolver(),
+            Settings.System.PEAK_REFRESH_RATE, Float.valueOf(refreshRateSettings[1]));
+        });
     }
 }
