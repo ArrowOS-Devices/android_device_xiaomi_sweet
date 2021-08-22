0d7557e450181f0e303ef25ee746a6c09899879a
daniml3 <danimoral1001@gmail.com>
2021-08-22 16:34:00 +0200

    sweet: block the SetRefreshRate builtin display function
    Signed-off-by: daniml3 <danimoral1001@gmail.com>
    Change-Id: I2fa6b3f834df79c32d22f5a057da2108cc9cacab

---
+++ hwcomposer-shim/Android.mk
@@ -0,0 +1,17 @@
+LOCAL_PATH := $(call my-dir)
+LIBSDMCORE := $(TARGET_OUT_VENDOR)/lib64/libsdmcore.so
+HWCOMPOSER := $(TARGET_OUT_VENDOR)/lib64/hw/hwcomposer.$(TARGET_BOARD_PLATFORM).so
+
+include $(CLEAR_VARS)
+LOCAL_MODULE := libsdmcore-patch
+LOCAL_SRC_FILES := $(LOCAL_MODULE)
+LOCAL_MODULE_CLASS := ETC
+LOCAL_VENDOR_MODULE := true
+LOCAL_REQUIRED_MODULES := hwcomposer.$(TARGET_BOARD_PLATFORM) libsdmcore
+
+LOCAL_POST_INSTALL_CMD := \
+    sed -i s/_ZN3sdm14DisplayBuiltIn14SetRefreshRateEjb/_ZN3sdm14DisplayBuiltIn14SetRefreshOrigEjb/g $(LIBSDMCORE); \
+    sed -i s/libdisplaydebug/libsdmcore-shim/g $(HWCOMPOSER); \
+    rm $(TARGET_OUT_VENDOR)/etc/$(LOCAL_MODULE)
+
+include $(BUILD_PREBUILT)
