141ab70a77d7376d021410ce9e93f8144df34982
daniml3 <danimoral1001@gmail.com>
2021-08-29 21:55:18 +0200

    sweet: import stock display calibration config
    Signed-off-by: daniml3 <danimoral1001@gmail.com>
    Change-Id: I9c2645119bbf9ba00f7b8b5a79588aa2294f3d8f

--- device.mk
+++ device.mk
@@ -233,6 +233,9 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     libsdmcore-shim
 
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/configs/display/qdcm_calib_data_xiaomi_k6_38_0c_0a_fhd_dsc_video_dsi_panel.xml:$(TARGET_COPY_OUT_VENDOR)/etc/qdcm_calib_data_xiaomi_k6_38_0c_0a_fhd_dsc_video_dsi_panel.xml
+
 # Media
 PRODUCT_PACKAGES += \
     libmm-omxcore \
