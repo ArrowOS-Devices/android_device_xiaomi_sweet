2a0f112f2f57bbcee6aa52f570fa9f02e25a508a
daniml3 <danimoral1001@gmail.com>
2021-10-07 20:15:48 +0200

    sweet: build_command: switch to BUILD_PREBUILT
    Change-Id: Idb0e77746cabef2f800b849707ccb47c61c46cd6

--- soong/build_command.go
+++ soong/build_command.go
@@ -4,8 +4,6 @@ import (
 	"fmt"
 	"io"
 
-	"github.com/google/blueprint"
-
 	"android/soong/android"
 )
 
@@ -27,9 +25,11 @@ type Module struct {
 	android.ModuleBase
 	properties moduleProperties
 	outputPath android.WritablePath
+	command    string
 }
 
 func (g *Module) GenerateAndroidBuildActions(ctx android.ModuleContext) {
+	productOut := ctx.Config().Getenv("OUT_DIR") + "/target/product/" + ctx.Config().DeviceName()
 	g.outputPath = android.PathForModuleGen(ctx)
 
 	if len(g.properties.Cmd) == 0 {
@@ -40,41 +40,29 @@ func (g *Module) GenerateAndroidBuildActions(ctx android.ModuleContext) {
 	command, err := android.ExpandNinjaEscaped(g.properties.Cmd, func(name string) (string, bool, error) {
 		switch name {
 		case "productOut":
-			return ctx.Config().Getenv("OUT_DIR") + "/target/product/" + ctx.Config().DeviceName(), false, nil
+			return productOut, false, nil
 		default:
 			return "", false, fmt.Errorf("unknown variable '%s'", name)
 		}
 	})
 
 	if err == nil {
-		g.runCommand(ctx, command)
+		g.command = fmt.Sprintf("(%s); rm -f %s/system/etc/%s", command, productOut, g.Name())
 	} else {
 		ctx.PropertyErrorf("cmd", "%s", err.Error())
 	}
 }
 
-func (g *Module) runCommand(ctx android.ModuleContext, command string) {
-	ruleParams := blueprint.RuleParams{
-		Command:     command,
-		CommandDeps: g.properties.Required,
-	}
-	rule := ctx.Rule(pctx, moduleName, ruleParams, nil...)
-	params := android.BuildParams{
-		Rule:        rule,
-		Description: " ",
-		Output:      g.outputPath,
-	}
-
-	ctx.Build(pctx, params)
-}
-
 func (g *Module) AndroidMk() android.AndroidMkData {
 	return android.AndroidMkData{
-		Include:    "$(BUILD_PHONY_PACKAGE)",
+		Include:    "$(BUILD_PREBUILT)",
 		OutputFile: android.OptionalPathForPath(g.outputPath),
 		Extra: []android.AndroidMkExtraFunc{
 			func(w io.Writer, output android.Path) {
-				fmt.Fprintln(w, "LOCAL_ADDITIONAL_DEPENDENCIES := ", g.outputPath.String())
+				fmt.Fprintln(w, "LOCAL_MODULE := ", g.Name())
+				fmt.Fprintln(w, "LOCAL_PREBUILT_MODULE_FILE := /dev/null")
+				fmt.Fprintln(w, "LOCAL_MODULE_CLASS := ETC")
+				fmt.Fprintln(w, "LOCAL_POST_INSTALL_CMD := ", g.command)
 			},
 		},
 	}
