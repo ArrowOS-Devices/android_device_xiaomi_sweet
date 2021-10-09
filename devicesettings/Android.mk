0f14aadfc70c9271ee67bbd18a40309b0181d6f3
daniml3 <danimoral1001@gmail.com>
2021-10-09 14:04:06 +0200

    sweet: devicesettings: refactor not to depend on external resources
    Signed-off-by: daniml3 <danimoral1001@gmail.com>
    Change-Id: Ifdc450bf28178bcd61f8cac09c51bb9ae0be1d56

--- devicesettings/Android.mk
+++ devicesettings/Android.mk
@@ -19,8 +19,7 @@ LOCAL_STATIC_ANDROID_LIBRARIES := \
 LOCAL_PROGUARD_FLAG_FILES := proguard.flags
 
 LOCAL_RESOURCE_DIR := \
-    $(LOCAL_PATH)/res \
-    $(TOP)/packages/resources/devicesettings/res
+    $(LOCAL_PATH)/res
 
 include frameworks/base/packages/SettingsLib/common.mk
 
