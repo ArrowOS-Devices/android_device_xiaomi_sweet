4cfc61e230077bd155536097c28917619d3c564a
daniml3 <daniel@danielml.dev>
2021-11-01 16:35:46 +0000

    sweet: shim libgui's surface constructor
    * Fixes loading the library lib-imsvideocodec.so
    
    Signed-off-by: daniml3 <daniel@danielml.dev>
    Change-Id: I0b88cde68f843d962492a898cae5cbaf11c60e67

--- extract-files.sh
+++ extract-files.sh
@@ -89,6 +89,8 @@ function blob_fixup() {
         $PATCHELF_TOOL --remove-needed "libmegface.so" "${2}"
         $PATCHELF_TOOL --add-needed "libshim_megvii.so" "${2}"
         ;;
+    system_ext/lib64/lib-imsvideocodec.so)
+        $PATCHELF_TOOL --add-needed "libgui-shim.so" "${2}"
     esac
 }
 
