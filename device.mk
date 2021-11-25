f68d58f9b59ab24a093bf76d9617b1af58d23117
AdarshGrewal <adarshgrewal@gmail.com>
2021-11-26 00:09:47 +0530

    sweet: fingerprint: Uprev to 2.3 HAL

--- device.mk
+++ device.mk
@@ -29,7 +29,7 @@ PRODUCT_COPY_FILES += \
 
 # Fingerprint
 PRODUCT_PACKAGES += \
-    android.hardware.biometrics.fingerprint@2.1-service.xiaomi_sm6150
+    android.hardware.biometrics.fingerprint@2.3-service.xiaomi_sm6150
 
 # Camera
 PRODUCT_PACKAGES += \
