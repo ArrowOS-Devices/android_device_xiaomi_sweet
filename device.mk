ace9bd248d0d01662da97c0eb47050ef7e566134
Bowgo Tsai <bowgotsai@google.com>
2022-09-05 12:33:41 +0000

    sweet: Removing gsi_keys.mk
    It's no longer required to install the compliance GSI
    public keys into the ramdisk. Those public keys just need
    to be included in the VTS test suite for GSI verification.
    
    Bug: 176869478
    Test: TreeHugger
    Change-Id: I26c4a1c7dba28a3c8cf88af64d8539fdc23a4f28

--- device.mk
+++ device.mk
@@ -4,9 +4,6 @@
 # SPDX-License-Identifier: Apache-2.0
 #
 
-# Installs gsi keys into ramdisk, to boot a GSI with verified boot.
-$(call inherit-product, $(SRC_TARGET_DIR)/product/gsi_keys.mk)
-
 # Enable updating of APEXes
 $(call inherit-product, $(SRC_TARGET_DIR)/product/updatable_apex.mk)
 
