2d0ce0bb703cf7377f507facb88167dceaafc92f
Arian <arian.kulmer@web.de>
2023-01-04 00:36:15 +0100

    sweet: rename camera face detection libs
    Change-Id: I50af944e9398eef352f72f4729999f8d0c556837

--- extract-files.sh
+++ extract-files.sh
@@ -90,6 +90,11 @@ function blob_fixup() {
     vendor/lib64/camera/components/com.qti.node.watermark.so)
         $PATCHELF_TOOL --add-needed "libpiex_shim.so" "${2}"
         ;;
+    vendor/lib64/hw/camera.qcom.so | vendor/lib64/libFaceDetectpp-0.5.2.so | vendor/lib64/libfacedet.so)
+        sed -i "s|libmegface.so|libfacedet.so|g" "${2}"
+        sed -i "s|libMegviiFacepp-0.5.2.so|libFaceDetectpp-0.5.2.so|g" "${2}"
+        sed -i "s|megviifacepp_0_5_2_model|facedetectpp_0_5_2_model|g" "${2}"
+        ;;
     esac
 }
 
