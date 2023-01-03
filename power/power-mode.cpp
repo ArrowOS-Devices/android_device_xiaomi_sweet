326a7dc018e97d1ef3c2773f8eb6399f7f3b1d93
Arian <arian.kulmer@web.de>
2023-01-04 00:12:27 +0100

    sweet: power: use xiaomi-touch to handle dt2w
    Change-Id: I73f95e32daf9e2c702662ac2c2c8182e6b8eb214

--- power/power-mode.cpp
+++ power/power-mode.cpp
@@ -17,6 +17,16 @@
 #include <aidl/android/hardware/power/BnPower.h>
 #include <android-base/file.h>
 #include <android-base/logging.h>
+#include <sys/ioctl.h>
+
+// defines from drivers/input/touchscreen/xiaomi/xiaomi_touch.h
+#define SET_CUR_VALUE 0
+#define Touch_Doubletap_Mode 14
+
+#define TOUCH_DEV_PATH "/dev/xiaomi-touch"
+
+#define TOUCH_MAGIC 0x5400
+#define TOUCH_IOC_SETMODE TOUCH_MAGIC + SET_CUR_VALUE
 
 namespace aidl {
 namespace google {
@@ -29,6 +39,9 @@ using ::aidl::android::hardware::power::Mode;
 
 bool isDeviceSpecificModeSupported(Mode type, bool* _aidl_return) {
     switch (type) {
+        case Mode::DOUBLE_TAP_TO_WAKE:
+            *_aidl_return = true;
+            return true;
         default:
             return false;
     }
@@ -36,6 +49,13 @@ bool isDeviceSpecificModeSupported(Mode type, bool* _aidl_return) {
 
 bool setDeviceSpecificMode(Mode type, bool enabled) {
     switch (type) {
+        case Mode::DOUBLE_TAP_TO_WAKE: {
+            int fd = open(TOUCH_DEV_PATH, O_RDWR);
+            int arg[2] = {Touch_Doubletap_Mode, enabled ? 1 : 0};
+            ioctl(fd, TOUCH_IOC_SETMODE, &arg);
+            close(fd);
+            return true;
+        }
         default:
             return false;
     }
