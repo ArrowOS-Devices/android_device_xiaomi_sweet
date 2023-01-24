e7ad911010476ccd56014fd92326b1dd2d495dcf
basamaryan <basam.aryan@gmail.com>
2023-01-24 16:34:33 +0000

    sweet: Move to common framework compatibility matrix
    Change-Id: I68c19da7ac477a70edb2737e8ac32f99b63b919f

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -17,8 +17,6 @@ TARGET_OTA_ASSERT_DEVICE := sweet,sweetin
 TARGET_PROVIDES_AUDIO_EXTNS := true
 
 # HIDL
-DEVICE_FRAMEWORK_COMPATIBILITY_MATRIX_FILE := $(DEVICE_PATH)/configs/hidl/device_framework_compatibility_matrix.xml
-DEVICE_FRAMEWORK_COMPATIBILITY_MATRIX_FILE += vendor/lineage/config/device_framework_matrix.xml
 DEVICE_MANIFEST_FILE += $(DEVICE_PATH)/configs/hidl/manifest.xml
 ODM_MANIFEST_SKUS += sweet
 ODM_MANIFEST_SWEET_FILES := \
