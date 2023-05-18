03131a2c2965f60a9adcd0d4654c2b0fe2a9fe58
Arian <arian.kulmer@web.de>
2023-05-18 12:40:30 +0200

    sweet: Switch to kernel config fragment
    Change-Id: I91d35f56e90d4849716df85afa9de2771d29f578

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -27,7 +27,7 @@ TARGET_INIT_VENDOR_LIB := //$(DEVICE_PATH):libinit_sweet
 TARGET_RECOVERY_DEVICE_MODULES := libinit_sweet
 
 # Kernel
-TARGET_KERNEL_CONFIG := sweet_defconfig
+TARGET_KERNEL_CONFIG += vendor/sweet.config
 
 # Partitions
 BOARD_FLASH_BLOCK_SIZE := 131072
